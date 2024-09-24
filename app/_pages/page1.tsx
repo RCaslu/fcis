858ff9bde6c07ffacd72e00042c30a6d54b87c3f
Lucas Araujo <lukas.scaa@gmail.com>
2024-09-24 15:52:44 +0100

    test

--- app/_pages/page1.tsx
+++ app/_pages/page1.tsx
@@ -9,7 +9,7 @@ const Page1 = () => {
           backgroundImage: "url('/01/BACKGROUND.png')", // Correct path to the image
           height: '1000px',
           backgroundSize: 'cover',  
-          backgroundPosition: 'center' 
+          backgroundPosition: 'center'
         }}
       >
         {/* Additional image */}
